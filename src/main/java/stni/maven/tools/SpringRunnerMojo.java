f9d93de803ab383555d4a49bce436eafdd3862d5
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2013-07-08 01:49:10 +0200

    invoke spring using reflection

--- src/main/java/stni/maven/tools/SpringRunnerMojo.java
+++ src/main/java/stni/maven/tools/SpringRunnerMojo.java
@@ -4,15 +4,18 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @goal runSpring
- * @requiresDependencyResolution compile
+ * @requiresDependencyResolution test
  */
 public class SpringRunnerMojo extends AbstractMojo {
+    private final static String APP_CONTEXT_CLASS = "org.springframework.context.support.FileSystemXmlApplicationContext";
+
     /**
      * @parameter expression="${project}"
      * @required
@@ -36,21 +39,38 @@ public class SpringRunnerMojo extends AbstractMojo {
         LogConfiguration.useLogConfig("logback-blue.xml");
 
         try {
-            MavenClasspathUtil.extendPluginClasspath(project.getCompileClasspathElements());
+            MavenClasspathUtil.extendPluginClasspath(project.getTestClasspathElements());
             System.setProperty("basedir", project.getBasedir().getAbsolutePath());
-            final FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:" + contextFile.getAbsolutePath());
-            context.start();
-            getLog().info("Started. Stopping spring context...");
-            context.stop();
-            getLog().info("Stopped.");
-        } catch (Exception e) {
-            if (failOnError) {
-                throw new MojoExecutionException("Problem running spring", e);
-            } else {
-                getLog().error("Problem running spring, but continuing", e);
-            }
+            runSpringUsingReflection();
+        } catch (Throwable e) {
+            handleException("Problem running spring.", e);
         } finally {
             LogConfiguration.useLogConfig("logback.xml");
+        }
+    }
+
+    private void runSpringUsingReflection() throws Throwable {
+        try {
+            getLog().info("Starting spring context...");
+            final Class<?> appContextClass = Thread.currentThread().getContextClassLoader().loadClass(APP_CONTEXT_CLASS);
+            final Constructor<?> constructor = appContextClass.getConstructor(String.class);
+            final Object appContext = constructor.newInstance("file:" + contextFile.getAbsolutePath());
+            getLog().info("Started. Stopping spring context...");
+            appContextClass.getMethod("stop").invoke(appContext);
+            getLog().info("Stopped.");
+        } catch (InvocationTargetException e) {
+            throw e.getCause();
+        } catch (Exception e) {
+            handleException("Problem starting spring. Assure that spring is in project's classpath and " +
+                    "'constructor " + APP_CONTEXT_CLASS + "(String)' is existing.", e);
+        }
+    }
+
+    private void handleException(String msg, Throwable e) throws MojoExecutionException {
+        if (failOnError) {
+            throw new MojoExecutionException(msg, e);
+        } else {
+            getLog().error(msg + " Continuing anyways...", e);
         }
     }
 }

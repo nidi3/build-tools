24dd88425d35557ea508fb346a4a718bb490a746
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2013-12-03 15:14:18 +0100

    add ConfirmationMojo

---
+++ src/main/java/stni/maven/tools/ConfirmationMojo.java
@@ -0,0 +1,36 @@
+package stni.maven.tools;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @goal confirmation
+ */
+public class ConfirmationMojo extends AbstractMojo {
+    /**
+     * @parameter expression="${prompt}"
+     * @required
+     */
+    private String prompt;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.print(prompt);
+        String in = readInput();
+        if (!in.equalsIgnoreCase("y") && !in.equalsIgnoreCase("yes")) {
+            throw new MojoExecutionException("User did not confirm question '" + prompt + "'");
+        }
+    }
+
+    private String readInput() throws MojoExecutionException {
+        try {
+            return new BufferedReader(new InputStreamReader(System.in)).readLine();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Problem reading input", e);
+        }
+    }
+}

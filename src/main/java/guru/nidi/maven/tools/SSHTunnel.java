13db0e6c9e284e952c505f9ccce718fe804b5fc6
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-14 01:57:28 +0200

    add license headers

--- src/main/java/guru/nidi/maven/tools/SSHTunnel.java
+++ src/main/java/guru/nidi/maven/tools/SSHTunnel.java
@@ -1,30 +1,26 @@
-/**
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
  *
- * Copyright  2000-2002,2004 The Apache Software Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package guru.nidi.maven.tools;
 
+import com.jcraft.jsch.Session;
 import org.apache.tools.ant.*;
-import org.apache.tools.ant.taskdefs.optional.ssh.*;
+import org.apache.tools.ant.taskdefs.optional.ssh.SSHBase;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import com.jcraft.jsch.*;
 
 /**
  * Creates an SSH tunnel that nested tasks can utilize to perform tasks on remote hosts.

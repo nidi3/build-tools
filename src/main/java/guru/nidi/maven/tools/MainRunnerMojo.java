ff189a6073ff0938aed2981237415f40d30c0624
Stefan Niederhauser <ghuder5@gmx.ch>
2015-03-13 18:13:17 +0100

    add MainRunnerMojo

---
+++ src/main/java/guru/nidi/maven/tools/MainRunnerMojo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.maven.tools;
+
+import java.lang.reflect.Method;
+
+/**
+ * @goal runMain
+ */
+public class MainRunnerMojo extends AbstractRunnerMojo {
+    /**
+     * @parameter expression="${mainClass}"
+     * @required
+     */
+    private String mainClass;
+    /**
+     * @parameter expression="${arguments}"
+     */
+    private String arguments;
+
+    @Override
+    public void run() throws Throwable {
+        final Class<?> main = Thread.currentThread().getContextClassLoader().loadClass(mainClass);
+        final Method mainMethod = main.getMethod("main", String[].class);
+        mainMethod.invoke(null, (Object) (arguments == null ? new String[0] : arguments.split(" ")));
+    }
+}
--- src/main/java/guru/nidi/maven/tools/SpringRunnerMojo.java
+++ src/main/java/guru/nidi/maven/tools/SpringRunnerMojo.java
@@ -15,31 +15,15 @@
  */
 package guru.nidi.maven.tools;
 
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
-
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 /**
  * @goal runSpring
- * @requiresDependencyResolution test
  */
-public class SpringRunnerMojo extends AbstractMojo {
+public class SpringRunnerMojo extends AbstractRunnerMojo {
     private final static String APP_CONTEXT_CLASS = "org.springframework.context.support.FileSystemXmlApplicationContext";
-
-    /**
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
-     */
-    private MavenProject project;
-
     /**
      * @parameter expression="${contextFile}"
      * @required
@@ -52,30 +36,11 @@ public class SpringRunnerMojo extends AbstractMojo {
      */
     private String profiles;
 
-    /**
-     * @parameter expression="${failOnError}"
-     */
-    private boolean failOnError;
-
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("******************** If using IntelliJ, consider using grep console plugin ********************");
-        LogConfiguration.useLogConfig("logback-blue.xml");
-
-        try {
-            MavenClasspathUtil.extendPluginClasspath(testClasspathElements());
-            System.setProperty("spring.profiles.active", profiles);
-            System.setProperty("basedir", project.getBasedir().getAbsolutePath());
-            runSpringUsingReflection();
-        } catch (Throwable e) {
-            handleException("Problem running spring.", e);
-        } finally {
-            LogConfiguration.useLogConfig("logback.xml");
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<String> testClasspathElements() throws DependencyResolutionRequiredException {
-        return project.getTestClasspathElements();
+    @Override
+    public void run() throws Throwable {
+        System.setProperty("spring.profiles.active", profiles);
+        System.setProperty("basedir", project.getBasedir().getAbsolutePath());
+        runSpringUsingReflection();
     }
 
     private void runSpringUsingReflection() throws Throwable {
@@ -92,14 +57,6 @@ public class SpringRunnerMojo extends AbstractMojo {
         } catch (Exception e) {
             handleException("Problem starting spring. Assure that spring is in project's classpath and " +
                     "'constructor " + APP_CONTEXT_CLASS + "(String)' is existing.", e);
-        }
-    }
-
-    private void handleException(String msg, Throwable e) throws MojoExecutionException {
-        if (failOnError) {
-            throw new MojoExecutionException(msg, e);
-        } else {
-            getLog().error(msg + " Continuing anyways...", e);
         }
     }
 }
